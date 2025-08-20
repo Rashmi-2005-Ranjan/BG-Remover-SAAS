a0ee95173ae6dad59dcea1e31d19e0bb625d3435
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-08-20 21:01:02 +0530

    Remove Bg response Class

---
+++ bg-remover-backend/src/main/java/in/clearifypro/Response/RemoveBgResponse.java
@@ -0,0 +1,17 @@
+package in.clearifypro.Response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RemoveBgResponse {
+    private boolean success;
+    private HttpStatus statusCode;
+    private Object data;
+}
