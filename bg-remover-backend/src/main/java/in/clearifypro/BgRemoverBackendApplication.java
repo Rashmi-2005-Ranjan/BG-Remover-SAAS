f16615d9df6642db06792112bd7d45310553b174
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-08-23 22:29:47 +0530

    Main Class and pom file

---
+++ bg-remover-backend/src/main/java/in/clearifypro/BgRemoverBackendApplication.java
@@ -0,0 +1,13 @@
+package in.clearifypro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgRemoverBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run ( BgRemoverBackendApplication.class , args );
+    }
+
+}
