219956c1120e93057356ca1715a184c51691a82c
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-08-28 18:24:13 +0530

    Feign Client Dependency added and configured it for using ClipDrop Api

--- bg-remover-backend/src/main/java/in/clearifypro/BgRemoverBackendApplication.java
+++ bg-remover-backend/src/main/java/in/clearifypro/BgRemoverBackendApplication.java
@@ -2,8 +2,10 @@ package in.clearifypro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class BgRemoverBackendApplication {
 
     public static void main(String[] args) {
---
+++ bg-remover-backend/src/test/java/in/clearifypro/BgRemoverBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package in.clearifypro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgRemoverBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
