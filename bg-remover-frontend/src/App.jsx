1a66e8d92919d5ebe43146112d085a34f1d58f6b
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-08-29 14:07:52 +0530

    main component app component and index page

--- bg-remover-frontend/src/App.jsx
+++ bg-remover-frontend/src/App.jsx
@@ -3,14 +3,31 @@ import Home from "./Pages/Home.jsx";
 import Footer from "./Components/Footer.jsx";
 import {Route, Routes} from "react-router-dom";
 import {Toaster} from "react-hot-toast";
+import UserSyncHandler from "./Components/UserSyncHandler.jsx";
+import Result from "./Pages/Result.jsx";
+import {RedirectToSignIn, SignedIn, SignedOut} from "@clerk/clerk-react";
+import BuyCredits from "./Pages/BuyCredits.jsx";
 
 const App = () => {
     return (
         <div>
+            <UserSyncHandler/>
             <Menubar/>
             <Toaster/>
             <Routes>
                 <Route path="/" element={<Home/>}/>
+                <Route path="/pricing" element={<BuyCredits/>}/>
+                {/*Protected Route*/}
+                <Route path="/result" element={
+                    <>
+                        <SignedIn>
+                            <Result/>
+                        </SignedIn>
+                        <SignedOut>
+                            <RedirectToSignIn/>
+                        </SignedOut>
+                    </>
+                }/>
             </Routes>
             <Footer/>
         </div>
